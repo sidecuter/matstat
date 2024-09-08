5ca4a38a6157f69f1a77f5030e34ecad7bf5c997
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2024-09-08 21:02:52 +0300

    Fix

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,15 @@ use crate::tasks::ms1::Ms1;
 #[component]
 pub fn App() -> impl IntoView {
     view! {
-        <Router>
-            <div class="min-h-screen min-w-full justify-items-center justify-center">
-                <Routes>
-                    <Route path="/" view=Ms1 />
-                </Routes>
-            </div>
-        </Router>
+        // <Router>
+        //     <div class="min-h-screen min-w-full justify-items-center justify-center">
+        //         <Routes>
+        //             <Route path="/" view=Ms1 />
+        //         </Routes>
+        //     </div>
+        // </Router>
+        <div class="min-h-screen min-w-full justify-items-center justify-center">
+            <Ms1/>
+        </div>
     }
 }
