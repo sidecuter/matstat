48177bd09589fca80555c8b4d7ebad3238a0baf4
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2024-12-09 21:15:33 +0300

    Simplify code by splitting it parts to separate functions

--- src/tasks/ms/ms1/formula.rs
+++ src/tasks/ms/ms1/formula.rs
@@ -1,10 +1,11 @@
 use leptos::*;
 use crate::models::func_data::FormulaData;
-
+use crate::models::table::Table;
+use crate::tasks::ms::ms1::dp::function_data;
 
 #[component]
 pub fn Formula(
-    conditions: ReadSignal<Vec<FormulaData>>,
+    data: ReadSignal<Table>,
     n: ReadSignal<i64>
 ) -> impl IntoView {
     view! {
@@ -26,58 +27,78 @@ pub fn Formula(
                     "["
                 </mo>
                 <mtable>
-                    {move || {
-                        conditions()
-                            .into_iter()
-                            .map(|fd| {
-                                view! {
-                                    <mtr>
-                                        <mtd>
-                                            {move || match fd.value {
-                                                0 => view! { <mn>"0"</mn> }.into_any(),
-                                                _ if fd.value / n() == 1 => {
-                                                    view! { <mn>"1"</mn> }.into_any()
-                                                }
-                                                _ => {
-                                                    view! {
-                                                        <mfrac>
-                                                            <mn>{move || fd.value}</mn>
-                                                            <mn>{n}</mn>
-                                                        </mfrac>
-                                                    }
-                                                        .into_any()
-                                                }
-                                            }} <mtext>", если"</mtext> <mspace width="5px" />
-                                            {move || {
-                                                fd.borders
-                                                    .left
-                                                    .is_some()
-                                                    .then(|| {
-                                                        view! {
-                                                            <mn>{move || fd.borders.left.unwrap().to_string()}</mn>
-                                                            <mo>"<"</mo>
-                                                        }
-                                                    })
-                                            }} <mi>"x"</mi>
-                                            {move || {
-                                                fd.borders
-                                                    .right
-                                                    .is_some()
-                                                    .then(|| {
-                                                        view! {
-                                                            <mo>"≤"</mo>
-                                                            <mn>{move || fd.borders.right.unwrap().to_string()}</mn>
-                                                        }
-                                                    })
-                                            }}
-                                        </mtd>
-                                    </mtr>
-                                }
-                            })
-                            .collect_view()
-                    }}
+                    {move || function_data(&data())
+                        .into_iter()
+                        .map(|fd| {
+                            formula_row(fd, n)
+                        })
+                        .collect_view()
+                    }
                 </mtable>
             </mrow>
         </math>
+    }
+}
+
+fn formula_row(
+    fd: FormulaData,
+    n: ReadSignal<i64>
+) -> impl IntoView {
+    view! {
+        <mtr>
+            <mtd>
+                {first_row_element(fd, n)}
+                <mtext>", если"</mtext>
+                <mspace width="5px" />
+                {left_row_element(fd)}
+                <mi>"x"</mi>
+                {right_row_element(fd)}
+            </mtd>
+        </mtr>
+    }
+}
+
+fn first_row_element(fd: FormulaData, n: ReadSignal<i64>) -> impl IntoView {
+    view! {
+        {move || match fd.value {
+            0 => view! { <mn>"0"</mn> }.into_any(),
+            _ if fd.value / n() == 1 => view! { <mn>"1"</mn> }.into_any(),
+            _ => view! {
+                    <mfrac>
+                        <mn>{move || fd.value}</mn>
+                        <mn>{n}</mn>
+                    </mfrac>
+                }.into_any()
+        }}
+    }
+}
+
+fn left_row_element(fd: FormulaData) -> impl IntoView {
+    view! {
+        {move || fd.borders
+            .left
+            .is_some()
+            .then(|| {
+                view! {
+                    <mn>{move || fd.borders.left.unwrap().to_string()}</mn>
+                    <mo>"<"</mo>
+                }
+            })
+        }
+    }
+}
+
+fn right_row_element(fd: FormulaData) -> impl IntoView {
+    view! {
+        {move || fd.borders
+            .right
+            .is_some()
+            .then(|| {
+                view! {
+                    <mo>"≤"</mo>
+                    <mn>{move || fd.borders.right.unwrap().to_string()}</mn>
+                }
+            })
+        }
     }
 }
